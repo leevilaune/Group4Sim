d415871cd8863a811a2db2ffad699cd0a429c031
leevivl <leevi.laune@metropolia.fi>
2025-09-16 13:13:28 +0300

    remove javafx from Main.java

--- src/main/java/fi/group4/project/Main.java
+++ src/main/java/fi/group4/project/Main.java
@@ -1,26 +1,6 @@
 package fi.group4.project;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) {
-        Label label = new Label("Hello, JavaFX!");
-        label.setFont(new Font("Times New Roman",20));
-        Scene scene = new Scene(label, 400, 200);
-
-        stage.setTitle("JavaFX Hello World");
-        stage.setScene(scene);
-        stage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch();
     }
 }

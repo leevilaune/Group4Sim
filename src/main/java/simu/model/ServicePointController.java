fe105de54d15f501d61c878a625c53373f225d13
leevivl <leevi.laune@metropolia.fi>
2025-09-16 19:26:17 +0300

    add ServicePointController to allow multiple ServicePoint objects in each task type

---
+++ src/main/java/simu/model/ServicePointController.java
@@ -0,0 +1,49 @@
+package simu.model;
+
+import eduni.distributions.ContinuousGenerator;
+import simu.framework.EventList;
+
+import java.util.Arrays;
+
+public class ServicePointController {
+
+    private ServicePoint[] servicePoints;
+    private ContinuousGenerator generator;
+    private EventList eventList;
+    private EventType type;
+
+    public ServicePointController(int amount, ContinuousGenerator generator, EventList eventList, EventType type){
+        this.servicePoints = new ServicePoint[amount];
+        this.generateServicePoints(amount,generator,eventList,type);
+    }
+    private void generateServicePoints(int amount,ContinuousGenerator generator, EventList eventList, EventType type){
+        for(int i = 0; i<amount;i++){
+            this.servicePoints[i] = new ServicePoint(generator,eventList,type);
+        }
+    }
+
+    public void addQueue(Customer a){
+        Arrays.stream(this.servicePoints)
+                .filter(sp -> !sp.isReserved())
+                .findFirst()
+                .ifPresent(sp -> sp.addQueue(a));
+    }
+    public boolean isReserved(){
+        final boolean[] anyIsFree = new boolean[1];
+        Arrays.stream(this.servicePoints)
+                .forEach(sp -> {
+                    if(!sp.isReserved()) anyIsFree[0] = true;
+                });
+        return anyIsFree[0];
+    }
+    public Customer removeQueue(){
+        Customer[] customer = new Customer[1];
+        Arrays.stream(this.servicePoints)
+                .filter(ServicePoint::isOnQueue)
+                .sorted()
+                .findFirst()
+                .ifPresent(sp -> {customer[0]=sp.removeQueue();
+                });
+        return customer[0];
+    }
+}

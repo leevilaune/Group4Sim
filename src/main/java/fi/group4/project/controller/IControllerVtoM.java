21692d39f218acaab93ccc7be7f6f3b5a6763465
leevivl <leevi.laune@metropolia.fi>
2025-10-06 19:23:06 +0300

    add combobox for distribution and listview for loading historical data

--- src/main/java/fi/group4/project/controller/IControllerVtoM.java
+++ src/main/java/fi/group4/project/controller/IControllerVtoM.java
@@ -2,7 +2,7 @@ package fi.group4.project.controller;
 
 /* interface for the UI */
 public interface IControllerVtoM {
-		public void startSimulation(int param1, int param2, int param3, int param4, int param5, long seed);
+		public void startSimulation(int param1, int param2, int param3, int param4, int param5, long seed, String distribution);
 		public void increaseSpeed();
 		public void decreaseSpeed();
 }
